ca044a5dbad0bb3477771db7fb207b129c53bb72
Khanvilkar <manav.khanvilkar@siemens.com>
2024-05-14 14:10:35 -0700

    only show relevant info for json

--- api_test.py
+++ api_test.py
@@ -1,8 +1,8 @@
 import requests
+import json
 
 base_url = "https://api.mouser.com/api/v2/"
 api_key = "2c18ba1d-7b17-4d2e-9e0f-34e05a5e674c"
-
     
 def search_parts_by_keyword_and_manufacturer(manufacturer_name, keyword, records, page_number, search_options, lang, return_type):
     search_endpoint = "search/keywordandmanufacturer"
@@ -23,7 +23,7 @@ def search_parts_by_keyword_and_manufacturer(manufacturer_name, keyword, records
         "Content-Type": "application/json",
         "Accept": ""
     }
-    headers["Accept"] = "application/" + return_type
+    headers["Accept"] = "tex/" + return_type
     response = requests.post(url, json=payload, headers=headers)
 
     if response.status_code == 200:
@@ -48,7 +48,7 @@ def search_parts_by_part_number_and_manufacturer(manufacturer_name, mouser_part_
         "Content-Type": "application/json",
         "Accept": ""
     }
-    headers["Accept"] = "application/" + return_type
+    headers["Accept"] = "text/" + return_type
     response = requests.post(url, json=payload, headers=headers)
 
     if response.status_code == 200:
@@ -59,6 +59,7 @@ def search_parts_by_part_number_and_manufacturer(manufacturer_name, mouser_part_
     
 def get_run_user_input():
     function_call = int(input("Enter 1 to search by keyword or 2 to search by part number: "))
+    return_type = input("Enter return data type (xml or json): ")
     if function_call == 1:
         manufacturer_name = input("Enter the manufacturer name: ")
         keyword = input("Enter the keyword: ")
@@ -66,19 +67,29 @@ def get_run_user_input():
         page_number = int(input("Enter the page number: "))
         search_options = input("Enter the search options: ")
         lang = input("Enter the search language: ")
-        return_type = input("Enter return data type (xml or json): ")
-        return search_parts_by_keyword_and_manufacturer(manufacturer_name, keyword, records, page_number, search_options, lang, return_type)
+        search_results = search_parts_by_keyword_and_manufacturer(manufacturer_name, keyword, records, page_number, search_options, lang, return_type)
+
     elif(function_call == 2):
         manufacturer_name = input("Enter the manufacturer name: ")
         mouser_part_number = input("Enter the Mouser part number: ")
         part_search_options = input("Enter the part search options: ")
-        return_type = input("Enter return data type (xml or json): ")
-        return search_parts_by_part_number_and_manufacturer(manufacturer_name, mouser_part_number, part_search_options, return_type)
+        search_results = search_parts_by_part_number_and_manufacturer(manufacturer_name, mouser_part_number, part_search_options, return_type)
+    
+    if(return_type == 'json'):
+        search_results_decoded = search_results.decode("utf-8")
+        search_results_json = json.loads(search_results_decoded)
+        part_data = search_results_json['SearchResults']['Parts'][0]
+        print("Manufacturer:", part_data['Manufacturer'])
+        print("Manufacturer Part Number:", part_data['ManufacturerPartNumber'])
+        print("Description:", part_data['Description'])
+        print("Price Breaks:")
+        for price_break in part_data['PriceBreaks']:
+            print("Quantity:", price_break['Quantity'], "| Price:", price_break['Price'], price_break['Currency'])
+    else:
+        print(search_results)
     
 
 def main():
-    search_results = get_run_user_input()
-    print(search_results)
-
+    get_run_user_input()
 if __name__ == "__main__":
     main()
