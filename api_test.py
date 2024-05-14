d3333c86da0b1509dd276a00228ea36682a3b2ab
Khanvilkar <manav.khanvilkar@siemens.com>
2024-05-14 10:37:12 -0700

    added api test file to test mouserapi

---
+++ api_test.py
@@ -0,0 +1,30 @@
+import requests
+
+base_url = "https://api.mouser.com/api/v2"
+
+api_key = "2c18ba1d-7b17-4d2e-9e0f-34e05a5e674c"
+
+search_endpoint = "/search/keywordandmanufacturer"
+
+url = base_url + search_endpoint
+
+payload = {
+    "SearchByPartMfrNameRequest": {
+        "manufacturerName": "Altech",
+        "mouserPartNumber": "845-008M5X20F",
+        "partSearchOptions": "Cartridge Fuses Fuse, Miniature, 5x20mm 0.08A, 250V, fast blow"
+    }
+}
+
+headers = {
+    "apiKey": api_key,
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print("Error:", response.status_code)
