0b3447f1103e3527fc5908b7832a3ab861d42a5a
Hershner <nathaniel.hershner@siemens.com>
2024-05-14 14:22:51 -0400

    XML functionality

--- api_test.py
+++ api_test.py
@@ -4,7 +4,7 @@ base_url = "https://api.mouser.com/api/v2/"
 api_key = "2c18ba1d-7b17-4d2e-9e0f-34e05a5e674c"
 
     
-def search_parts_by_keyword_and_manufacturer(manufacturer_name, keyword, records, page_number, search_options, lang):
+def search_parts_by_keyword_and_manufacturer(manufacturer_name, keyword, records, page_number, search_options, lang, return_type):
     search_endpoint = "search/keywordandmanufacturer"
     url = base_url + search_endpoint + "?apiKey=" + api_key
 
@@ -20,9 +20,10 @@ def search_parts_by_keyword_and_manufacturer(manufacturer_name, keyword, records
     }
 
     headers = {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Accept": ""
     }
-
+    headers["Accept"] = "application/" + return_type
     response = requests.post(url, json=payload, headers=headers)
 
     if response.status_code == 200:
@@ -31,7 +32,7 @@ def search_parts_by_keyword_and_manufacturer(manufacturer_name, keyword, records
     else:
         return {"Error": response.status_code}
     
-def search_parts_by_part_number_and_manufacturer(manufacturer_name, mouser_part_number, part_search_options):
+def search_parts_by_part_number_and_manufacturer(manufacturer_name, mouser_part_number, part_search_options, return_type):
     search_endpoint = "search/partnumberandmanufacturer"
     url = base_url + search_endpoint + "?apiKey=" + api_key
 
@@ -44,13 +45,14 @@ def search_parts_by_part_number_and_manufacturer(manufacturer_name, mouser_part_
     }
 
     headers = {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Accept": ""
     }
-
+    headers["Accept"] = "application/" + return_type
     response = requests.post(url, json=payload, headers=headers)
 
     if response.status_code == 200:
-        data = response.json()
+        data = response.content
         return data
     else:
         return {"Error": response.status_code}
@@ -64,20 +66,19 @@ def get_run_user_input():
         page_number = int(input("Enter the page number: "))
         search_options = input("Enter the search options: ")
         lang = input("Enter the search language: ")
-        return search_parts_by_keyword_and_manufacturer(manufacturer_name, keyword, records, page_number, search_options, lang)
+        return_type = input("Enter return data type (xml or json): ")
+        return search_parts_by_keyword_and_manufacturer(manufacturer_name, keyword, records, page_number, search_options, lang, return_type)
     elif(function_call == 2):
         manufacturer_name = input("Enter the manufacturer name: ")
         mouser_part_number = input("Enter the Mouser part number: ")
         part_search_options = input("Enter the part search options: ")
-        return search_parts_by_part_number_and_manufacturer(manufacturer_name, mouser_part_number, part_search_options)
+        return_type = input("Enter return data type (xml or json): ")
+        return search_parts_by_part_number_and_manufacturer(manufacturer_name, mouser_part_number, part_search_options, return_type)
     
 
 def main():
     search_results = get_run_user_input()
-    if "Error" in search_results:
-        print("Error:", search_results["Error"])
-    else:
-        print(search_results)
+    print(search_results)
 
 if __name__ == "__main__":
     main()
